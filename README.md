24513cffa0446d92f3093c12dceb90b0e3f67980
basti <basti.hoffmeister@gmx.de>
2022-12-29 19:28:56 +0100

    Implementing Deep Q Learning AI

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ A simple game for my daughter.
 # References
 
 - [pygame tutorial](https://realpython.com/pygame-a-primer/)
-- [Deep Reinforcement Learning](https://towardsdatascience.com/how-to-teach-an-ai-to-play-games-deep-reinforcement-learning-28f9b920440a)
+- [Deep Reinforcement Learning](https://towardsdatascience.com/how-to-teach-an-ai-to-play-games-deep-reinforcement-learning-28f9b920440a) [https://github.com/maurock/snake-ga](Code)
 - [Reinforcement Learning: Flappy Bird](https://www.toptal.com/deep-learning/pytorch-reinforcement-learning-tutorial)
+- [Multi Agent Reinforcement Learning](https://www.nature.com/articles/s41586-019-1724-z.epdf?author_access_token=lZH3nqPYtWJXfDA10W0CNNRgN0jAjWel9jnR3ZoTv0PSZcPzJFGNAZhOlk4deBCKzKm70KfinloafEF1bCCXL6IIHHgKaDkaTkBcTEv7aT-wqDoG1VeO9-wO3GEoAMF9bAOt7mJ0RWQnRVMbyfgH9A%3D%3D)
---
+++ poc/poc_ai.py
@@ -0,0 +1,69 @@
+# https://github.com/maurock/snake-ga/blob/master/DQN.py
+
+import random
+import numpy as np
+import pandas as pd
+from operator import add
+import collections
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import copy
+DEVICE = 'cpu' # 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class DQNAgent(torch.nn.Module):
+    def __init__(self, params):
+        super().__init__()
+        self.reward = 0
+        self.gamma = 0.9 
+        self.dataframe = pd.DataFrame()
+        self.short_memory = np.array([])
+        self.agent_target = 1
+        self.agent_predict = 0
+        self.learning_rate = params['learning_rate']        
+        self.epsilon = 1
+        self.actual = []
+        self.first_layer = params['first_layer_size']
+        self.second_layer = params['second_layer_size']
+        self.third_layer = params['third_layer_size']
+        self.memory = collections.deque(maxlen=params['memory_size'])
+        self.weights = params['weights_path']
+        self.load_weights = params['load_weights']
+        self.optimizer = None
+        self.network()
+
+    def network(self):
+    # Layers
+        self.f1 = nn.Linear(11, self.first_layer)
+        self.f2 = nn.Linear(self.first_layer, self.second_layer)
+        self.f3 = nn.Linear(self.second_layer, self.third_layer)
+        self.f4 = nn.Linear(self.third_layer, 3)
+        # weights
+        if self.load_weights:
+            self.model = self.load_state_dict(torch.load(self.weights))
+            print("weights loaded")
+    
+    def forward(self, x):
+        x = F.relu(self.f1(x))
+        x = F.relu(self.f2(x))
+        x = F.relu(self.f3(x))
+        x = F.softmax(self.f4(x), dim=-1)
+        return x
+    
+    def get_state(self, game, player, food):
+        """
+        Return the state.
+        The state is a numpy array of 6 values, representing:
+            - x,y-Position of the agent/player
+            - x,y-position of the closest food
+            - x,y-position of the second closest food
+        """
+        state = [
+            player.rect.centerx,
+            player.rect.centery,
+            getStateNearest(player, enemies, 3)
+        ]
+        return np.asarray(state)
+
+
